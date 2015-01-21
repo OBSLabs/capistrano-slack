885c7748f4a272291482936c35c135e7adf91ada
Dmitry Novotochinov <trybeee@gmail.com>
2015-01-21 11:50:22 -0800

    change activesupport requires to prevent #capture method override

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -2,17 +2,18 @@ require 'capistrano'
 require 'capistrano/log_with_awesome'
 require 'json'
 require 'net/http'
-require 'active_support/all'
+require 'active_support/core_ext/string/multibyte'
+require 'active_support/multibyte/chars'
 # TODO need to handle loading a bit beter. these would load into the instance if it's defined
 module Capistrano
   module Slack
-  
+
       def payload(announcement)
       {
         'channel' => fetch(:slack_room),
-        'username' => fetch(:slack_username, ''), 
-        'text' => announcement, 
-        'icon_emoji' => fetch(:slack_emoji, '') 
+        'username' => fetch(:slack_username, ''),
+        'text' => announcement,
+        'icon_emoji' => fetch(:slack_emoji, '')
         }.to_json
       end
 
@@ -24,12 +25,12 @@ module Capistrano
           http.verify_mode = OpenSSL::SSL::VERIFY_PEER
           request = Net::HTTP::Post.new(uri.request_uri)
           request.set_form_data(:payload => payload(message))
-          http.request(request) 
-        rescue SocketError => e 
+          http.request(request)
+        rescue SocketError => e
            puts "#{e.message} or slack may be down"
          end
       end
-      def slack_defaults 
+      def slack_defaults
         if fetch(:slack_deploy_defaults, true) == true
           before 'deploy', 'slack:starting'
           before 'deploy:migrations', 'slack:starting'
@@ -41,7 +42,7 @@ module Capistrano
       def self.extended(configuration)
         configuration.load do
 
-          before('deploy') do 
+          before('deploy') do
             slack_defaults
           end
 
@@ -59,7 +60,7 @@ module Capistrano
                 "#{announced_deployer} is deploying #{slack_application} to #{fetch(:stage, 'production')}"
               end
 
-              slack_connect(msg)  
+              slack_connect(msg)
               set(:start_time, Time.now)
             end
             task :finished do
@@ -68,7 +69,7 @@ module Capistrano
                 announced_deployer = fetch(:deployer)
                 start_time = fetch(:start_time)
                 elapsed = Time.now.to_i - start_time.to_i
-                msg = "#{announced_deployer} deployed #{slack_application} successfully in #{elapsed} seconds." 
+                msg = "#{announced_deployer} deployed #{slack_application} successfully in #{elapsed} seconds."
                 slack_connect(msg)
               rescue Faraday::Error::ParsingError
                 # FIXME deal with crazy color output instead of rescuing
@@ -85,4 +86,4 @@ end
 if Capistrano::Configuration.instance
   Capistrano::Configuration.instance.extend(Capistrano::Slack)
 end
-  
+
